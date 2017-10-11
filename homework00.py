06bcbbb288512b4f8b5ea1e259fa7e24ec4d8985
aiiaaiiaaiia <ntwtktwt@gmail.com>
2017-10-11 14:30:42 +0700

    first commit

--- homework00.py
+++ homework00.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python2
-inp = int(input())
-fi = int(input())
-se = int(input())
-if inp == 1:
-        print('%.2f'%(fi+se))
-if inp == 2:
-        print('%.2f'%(fi-se))
+import rospy
+if __name__=='__main__':
+	rospy.init_node('Net_kak')
+	inp = int(input())
+	fi = int(input())
+	se = int(input())
+	if inp == 1:
+		print('%.2f'%(fi+se))
+	if inp == 2:
+		print('%.2f'%(fi-se))
